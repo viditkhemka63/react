59640c1784cab897164b68b8382a95a749eb9c44
vidit <viditkhemka63>
2021-07-06 14:21:31 +0530

    useEffect hook

--- hooks1/src/App.js
+++ hooks1/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Card from "./ components/card";
+import { useEffect, useState } from "react";
 
+function effect() {
+  console.log("use Effect called");
+}
 function App() {
+  const [count, setCount] = useState(1);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://dev.to/search/feed_content?per_page=60&page=0&class_name=Article&search_fields=react"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setData(res.result);
+      });
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div onClick={() => setCount(count + 1)} className="container">
+        {/* <button onClick={() => setCount(count + 1)}>test1</button> */}
+
+        {data &&
+          data.map((row) => {
+            return (
+              <Card
+                title={row.title}
+                likes={row.public_reactions_count}
+                comments={row.comments_count}
+              />
+            );
+          })}
+      </div>
+    </>
   );
 }
 
